d92fd4989a1b7d9b931512fcc43a337b4829e3ad
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-02-06 20:14:34 -0300

    feat: create `sum` between two numbers and print an integer value

--- 06-02-2024/Program.fs
+++ 06-02-2024/Program.fs
@@ -1,6 +1,19 @@
 ﻿let helloWorld () = printfn "Hello World!"
 
+let sum numberOne numberTwo = numberOne + numberTwo
+
 [<EntryPoint>]
-let main argv =
+let main args =
+    // Call the helloWord function
     helloWorld ()
+
+    // Force type integer to value constant
+    let value: int = 10
+    let otherValue: int = value + 4
+
+    // The "%i" print an integer value in printfn
+    // The sum function has priority over and it's
+    // been called first
+    printfn "The result is %i" (sum value otherValue)
+
     0
--- 06-02-2024/obj/Debug/net8.0/06-02-2024.AssemblyInfo.fs
+++ 06-02-2024/obj/Debug/net8.0/06-02-2024.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("06-02-2024")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8731a61248dfdf4925dea26cb7d959bdd2caf7e")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787310e623c54ab4f2b5387ddba92237da075526")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("06-02-2024")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("06-02-2024")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
