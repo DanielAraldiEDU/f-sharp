d5c277b061360164520845045e19b318415c3beb
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-02-06 21:37:26 -0300

    temp: small adjusts comment

--- 06-02-2024/Program.fs
+++ 06-02-2024/Program.fs
@@ -47,7 +47,7 @@ let main args =
     printfn "Value without signal: %i" (moduleWithoutSignal anotherValue)
     printfn "Media between these two numbers is: %i" (mediaBetweenTwoNumbers value otherValue)
     printfn "Show less value between three numbers is: %i" (lessValueBetweenThreeNumbers value otherValue anotherValue)
-    // The "%i" print an string value in printfn
+    // The "%s" print an string value in printfn
     printfn "Show which triangle type is: %s" (showTriangleType value value value)
 
     0
